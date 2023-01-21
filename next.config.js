c176e3baba1cd7f4c9278f553b002f7fcdd28818
Robert Oblesniuc <rob.oblesniuc@gmail.com>
2023-01-22 00:27:56 +0200

    Implemented monaco-editor and yjs + y-monaco for real-time collaborative editing! Need to fix awarenessgit add .

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
-const NextConfig = {
+const nextConfig = {
+  webpack: (config) => {
+    config.plugins.push(new MonacoWebpackPlugin({ filename: "static/[name].worker.js" }));
+    return config;
+  },
   reactStrictMode: true,
+  transpilePackages: ["monaco-editor"],
 };
 
-module.exports = NextConfig;
+module.exports = nextConfig;
