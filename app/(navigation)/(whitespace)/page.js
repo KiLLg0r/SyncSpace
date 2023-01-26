e9d1f24f17007da680a7f87caf952d6bcc7576ec
George Stanga <georgestanga39@gmail.com>
2023-01-26 16:12:36 +0200

    Started landing page

--- app/(navigation)/(whitespace)/page.js
+++ app/(navigation)/(whitespace)/page.js
@@ -1,7 +1,29 @@
 import "../../../styles/globals.css";
+import styles from "./Home.module.scss";
+import Planet from "../../../public/planet.svg";
 
 const Home = () => {
-  return <h1>Home page</h1>;
+  return (
+    <div className={styles.home}>
+      <main className={styles.main}>
+        <div className={styles.text}>
+          <div className={styles.maintext1}>TOGETHER</div>
+          <div className={styles.maintext2}>
+            WE SHIP <span>YOU</span>
+          </div>
+          <div className={styles.maintext3}>
+            TO THE <span>NEXT</span>
+          </div>
+          <div className={styles.maintext4}>LEVEL</div>
+        </div>
+        <div className={styles.planetContainer}>
+          <Planet className={styles.planet} />
+        </div>
+      </main>
+
+      <div className={styles.features}></div>
+    </div>
+  );
 };
 
 export default Home;
